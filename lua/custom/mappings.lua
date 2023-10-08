8fc8a28fbbc4916f0d0425800eb0ac6e7102003a
revviac <viacheslav.ponomarev.0@gmail.com>
2023-10-08 21:02:37 +0600

    Add custom options

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,22 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+M.disabled = {
+  n = {
+    ["Q"] = "",
+    ["q:"] = "",
+  }
+}
+
+-- more keybinds!
+
+return M
